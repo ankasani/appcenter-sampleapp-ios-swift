8f56d5652365fc1be7d2bbdad63f0fe0a4ea9a86
sum0999 <sum@microsoft.com>
2019-03-27 19:18:24 +0530

    Update appcenter-post-build.sh

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -1,15 +1,1 @@
 #!/usr/bin/env bash
-
-printenv
-
-echo "Testing if this is running"
-
-
-
-echo "Build.SourceVersion = "$BUILD_SOURCEVERSION
-
-
-
-echo "RUNNING_IN_APP_CENTER = "$RUNNING_IN_APP_CENTER
-
-echo $APPCENTER_OUTPUT_DIRECTORY
