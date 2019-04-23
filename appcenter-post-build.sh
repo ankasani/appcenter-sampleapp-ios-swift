9f17baf14a93caced10f9dbe4a5dbdada942d69a
sum0999 <sum@microsoft.com>
2019-04-23 17:25:54 +0530

    Update appcenter-post-build.sh

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env bash
+
+xcodebuild -sdk iphonesimulator -workspace $APPCENTER_SOURCE_DIRECTORY/sampleapp-ios-swift.xcodeproj/project.xcworkspace -scheme sampleapp-ios-swift test -destination "platform=iOS Simulator,name=iPhone 6"
+
 bash <(curl -s https://codecov.io/bash) -t 4fea6cba-071e-4832-8c6f-7fe7ee4bf768
